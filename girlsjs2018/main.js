cd096a8fabce25608d0c8f44753df67a54e19a49
martawoskowiak <marta.woskowiak@gmail.com>
2023-06-25 14:01:00 +0200

    'A jednak się kręci' task from girls.js training

---
+++ girlsjs2018/main.js
@@ -0,0 +1,42 @@
+let carousel = document.querySelector('.carousel');
+let stage = document.querySelector('.carousel-stage');
+let prev = document.querySelector('.carousel-prev');
+let next = document.querySelector('.carousel-next');
+let slide = document.querySelectorAll('.single-slide');
+
+let slideWidth = slide[0].clientWidth;
+console.log(slideWidth);
+
+let currentIndex = 0;
+let slidesNumber = slide.length - 1;
+
+function goToSlide(index) {
+    if (index < 0) {
+        index = slidesNumber;
+    } else if (index > slidesNumber){
+        index = 0;
+    }
+    carousel.style.left = index * (-slideWidth);
+    currentIndex = index;
+}
+
+function slideToNext() {
+    goToSlide(currentIndex + 1);
+
+}
+
+function slideToPrev() {
+    goToSlide(currentIndex - 1);
+}
+function bindEvents() {
+    prev.addEventListener('click', slideToPrev);
+    next.addEventListener('click', slideToNext);
+}
+
+bindEvents();
+
+function autorotate() {
+    setInterval(slideToNext, 4000);
+}
+
+autorotate();
